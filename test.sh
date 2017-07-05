1e3c70e343bff9a9ffad5b7c683b04a66a7da584
O. Lissenberg <olissenberg@rsm.nl>
2017-07-05 02:19:16 +0200

    Upgrade to Ansible 2.3.1.0 and PostgreSQL version 9.4

--- test.sh
+++ test.sh
@@ -2,17 +2,17 @@
 
 CURRENT_DIR=${PWD}
 TMP_DIR=/tmp/ansible-test
-mkdir -p $TMP_DIR 2> /dev/null
+mkdir -p ${TMP_DIR} 2> /dev/null
 
 # Create hosts inventory
-cat << EOF > $TMP_DIR/hosts
+cat << EOF > ${TMP_DIR}/hosts
 [webservers]
 localhost ansible_connection=local
 EOF
 
-# Create group_vars for the webservers
-mkdir -p $TMP_DIR/group_vars 2> /dev/null
-cat << EOF > $TMP_DIR/group_vars/webservers
+# Create group_vars for the web servers
+mkdir -p ${TMP_DIR}/group_vars 2> /dev/null
+cat << EOF > ${TMP_DIR}/group_vars/webservers
 
 postgresql_default_auth_method: "md5"
 
@@ -74,33 +74,33 @@ EOF
 # \du
 
 # Create Ansible config
-cat << EOF > $TMP_DIR/ansible.cfg
+cat << EOF > ${TMP_DIR}/ansible.cfg
 [defaults]
-roles_path = $CURRENT_DIR/../
+roles_path = ${CURRENT_DIR}/../
 host_key_checking = False
 EOF
 
 # Create playbook.yml
-cat << EOF > $TMP_DIR/playbook.yml
+cat << EOF > ${TMP_DIR}/playbook.yml
 ---
 
 - hosts: webservers
   gather_facts: yes
-  sudo: yes
+  become: yes
 
   roles:
     - ansible-postgresql
 EOF
 
-export ANSIBLE_CONFIG=$TMP_DIR/ansible.cfg
+export ANSIBLE_CONFIG=${TMP_DIR}/ansible.cfg
 
 # Syntax check
-ansible-playbook $TMP_DIR/playbook.yml -i $TMP_DIR/hosts --syntax-check
+ansible-playbook ${TMP_DIR}/playbook.yml -i ${TMP_DIR}/hosts --syntax-check
 
 # First run
-ansible-playbook $TMP_DIR/playbook.yml -i $TMP_DIR/hosts
+ansible-playbook ${TMP_DIR}/playbook.yml -i ${TMP_DIR}/hosts
 
 # Idempotence test
-# ansible-playbook $TMP_DIR/playbook.yml -i $TMP_DIR/hosts | grep -q 'changed=0.*failed=0' \
-# 	&& (echo 'Idempotence test: pass' && exit 0) \
-# 	|| (echo 'Idempotence test: fail' && exit 1)
+# ansible-playbook ${TMP_DIR}/playbook.yml -i ${TMP_DIR}/hosts | grep -q 'changed=0.*failed=0' \
+#   && (echo 'Idempotence test: pass' && exit 0) \
+#   || (echo 'Idempotence test: fail' && exit 1)
